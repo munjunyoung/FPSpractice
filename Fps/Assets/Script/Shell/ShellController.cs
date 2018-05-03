1b16f089795d23417b5ab32c626a291ff3371068
Monzon <sswsswsswss@gmail.com>
2018-05-03 20:15:17 +0900

    initial project version

---
+++ Fps/Assets/Script/Shell/ShellController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellController : MonoBehaviour {
+
+    public GameObject explosionEffectPrefab;
+    public Vector3 dir;
+    public Vector3 position;
+	// Use this for initialization
+	void Start () {
+        Destroy(this.gameObject, 5f);
+	}
+
+    // Update is called once per frame
+    private void Update()
+    {
+        GetComponent<Rigidbody>().AddForceAtPosition(dir.normalized*20000f, position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject explosionEffect = Instantiate(explosionEffectPrefab);
+        explosionEffect.transform.position = this.transform.position;
+
+        Destroy(this.gameObject);
+    }
+}
