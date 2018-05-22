b8309752308e389e1a6437cefa5eb40ec199bf42
Monzon <sswsswsswss@gmail.com>
2018-05-22 16:04:59 +0900

    all add

---
+++ Fps/Assets/Script/03Ingame/Player/PlayerMovement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Client;
+public class PlayerMovement : MonoBehaviour {
+
+    //Move
+    private float moveSpeed = 10f;
+    private float moveMaxSpeed = 50f;
+    private Vector3 playerMoving;
+
+    //Rotation
+    public float rotationX = 0f;
+    public float rotationY = 0f;
+    private float rotationSens = 100f;
+
+    private float rotationMinY = -35f;
+    private float rotationMaxY = 35f;
+
+    public GameObject camObject;
+    public GameObject centerObject;
+
+    Animator playerAnimator;
+
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        transform.position = new Vector3(ClientNetworkManager.playerPos.x, ClientNetworkManager.playerPos.y, ClientNetworkManager.playerPos.z);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Move();
+        PlayerTurn();
+    }
+    private void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        playerMoving = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            transform.Translate(playerMoving * moveMaxSpeed*Time.deltaTime);
+        else
+            transform.Translate(playerMoving * moveSpeed*Time.deltaTime);
+
+        PlayerAnim();
+
+    }
+
+    private void PlayerTurn()
+    {
+        rotationX += Input.GetAxis("Mouse X") * rotationSens * Time.deltaTime;
+        rotationY += Input.GetAxis("Mouse Y") * rotationSens * Time.deltaTime;
+        rotationY = Mathf.Clamp(rotationY, rotationMinY, rotationMaxY);
+        
+        transform.localEulerAngles = new Vector3(0f, rotationX, 0f);
+        centerObject.transform.localEulerAngles = new Vector3(-rotationY,0f, 0f);
+        camObject.transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0f);
+    }
+    
+
+    void PlayerAnim()
+    {
+        if (Mathf.Abs(playerMoving.x) < 0.1f && Mathf.Abs(playerMoving.z) < 0.1f)
+        {
+            playerAnimator.SetTrigger("Idle");
+        }
+        else
+        {
+            playerAnimator.SetTrigger("Run");
+        }
+        
+        if (Input.GetMouseButton(0))
+        {
+            playerAnimator.SetTrigger("Shoot");
+        }
+    }
+}
---
+++ Fps/Assets/Script/03Ingame/Player/PlayerShooting.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+    public GameObject TargetObject; // 조준점 타겟 오브젝트; raycasting
+    public Transform fireTransform;
+    public Rigidbody shellPrefab;
+    public GameObject[] aims;
+
+    float shootCooltime = 0.5f;
+    float reboundValue = 0f;
+	
+	void Update () {
+        PlayerShootFunc();
+        returnAim();
+    }
+
+    void PlayerShootFunc()
+    {
+        shootCooltime += Time.deltaTime;
+        if (shootCooltime > 0.1f)
+        {
+            if(Input.GetMouseButton(0))
+            {
+                ShellIns();
+                shootCooltime = 0f;
+            }
+        }
+    }
+
+    //Create Shell Func
+    void ShellIns() 
+    {
+        Rigidbody shellInstance = Instantiate(shellPrefab);
+        shellInstance.transform.position = fireTransform.position;
+        shellInstance.transform.LookAt(TargetObject.transform.position);
+        //shellInstance.velocity = fireForce * TargetObject.transform.forward;
+
+        //실제 총알 반동
+        Vector2 reboundCircle = Random.insideUnitCircle * reboundValue; //x, y circle 랜덤값
+        TargetObject.transform.localPosition = new Vector3(reboundCircle.x, reboundCircle.y, TargetObject.transform.localPosition.z);
+        Vector3 shellDir = TargetObject.transform.position - shellInstance.transform.position; // 방향설정
+        shellInstance.GetComponent<ShellController>().dir = shellDir;
+        shellInstance.GetComponent<ShellController>().position = shellInstance.transform.position;
+
+        shootReaction();
+    }
+    // Aim..
+    void shootReaction() // 에임이미지함수
+    {
+        //에임 이미지 벌어지게하기
+        foreach (GameObject aim in aims)
+        {
+            if (aim.transform.localPosition.x < 25)
+            {
+                aim.transform.Translate(Vector3.right * 7f);
+                reboundValue += 1f;
+            }
+        }
+        GetComponent<PlayerMovement>().rotationY+= 0.5f;
+        GetComponent<PlayerMovement>().rotationX += Random.Range(-1.0f, 1.0f);
+    }
+
+    void returnAim() // 에임회복함수
+    {
+        foreach (GameObject aim in aims)
+        {
+            if (aim.transform.localPosition.x > 0)
+            {
+                aim.transform.localPosition = Vector3.Lerp(aim.transform.localPosition, new Vector3(1f, 0, 0), Time.deltaTime*2f);
+                reboundValue = Mathf.Lerp(reboundValue, 0f, Time.deltaTime);
+            }
+        }
+    }
+}
